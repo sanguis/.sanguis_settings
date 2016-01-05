d12a44376d2c98fc439314f43ddb9d44c627ebbc
Josh Beauregard <josh.beauregard@knectar.com>
2016-01-05 14:23:26 -0500

    fixed date

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,10 @@
 #! /bin/sh
 
-d=`date`
+d=`date --date="%Y-%m-%dS%H:%M:%S"`
 tmp="/tmp/dots_$d"
 mkdir $tmp
 cd $tmp
 
-curl -O https://opscode-omnibus-packages.s3.amazonaws.com/debian/6/x86_64/chef_12.6.0-1_amd64.deb
-
-sudo dpkg  -i chef_12.6.0-1_amd64.deb
+curl -O https://opscode-omnibus-packages.s3.amazonaws.com/debian/6/x86_64/chef_12.6.0-1_amd64.deb |sudo dpkg -i 
 
 
