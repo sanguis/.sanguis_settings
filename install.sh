2505dddaefd94d37a094be751cc111dd31bfaa68
Josh Beauregard <josh.beauregard@knectar.com>
2016-01-22 11:52:48 -0500

    Fixing $P

--- install.sh
+++ install.sh
@@ -1,28 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-P=`pwd`
+P="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source $P/update.sh
-if [ -e ~/.szhrc ] then
+if [ -e ~/.szhrc ]
+then
   ZSHDATA=`cat ~/.zshrc`
   mv $HOME/.zshrc $HOME/.zshrc_old
   rm $HOME/.zshrc
-end
+fi
 
 echo $ZSHDATA >> $P/zshrc
 
 # create symlinks
 # todo: add ifs
-ln -s $P/vimrc $HOME/.vimrc;
-ln -s $P/.vim $HOME/.vim;
-ln -s $P/zshrc $HOME/.zshrc;
-ln -s $P/tmux.conf $HOME/.tmux.conf;
-ln -s $P/gitconfig $HOME/.gitconfig;
-ln -s $P/prezto $HOME/.zprezto;
+ln -s $P/vimrc $HOME/.vimrc
+ln -s $P/.vim $HOME/.vim
+ln -s $P/zshrc $HOME/.zshrc
+ln -s $P/tmux.conf $HOME/.tmux.conf
+ln -s $P/gitconfig $HOME/.gitconfig
+ln -s $P/prezto $HOME/.zprezto
 
 update_ss
-# setup prezto
+setup prezto
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+source $HOME/.zshrc
--- zshrc
+++ zshrc
@@ -6,3 +6,7 @@ source $HOME/.zprezto/init.zsh
 alias vhost="sudo ~/bin/vhost"
 alias vi="vim -g"
 alias new-module="sh ~/src/drupal-new-module/new-module.sh"
+
+
+
+
