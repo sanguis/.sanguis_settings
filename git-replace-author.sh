1cd2ee56d07159d324cf5e5d934716be42107e94
Josh Beauregard <sanguis@users.noreply.github.com>
2021-09-20 09:16:33 -0400

    fixed help formattting

--- git-replace-author.sh
+++ git-replace-author.sh
@@ -3,32 +3,31 @@
 # often on this repo I have used various corporate accounts to commit and I need to fix that.
 # original snip found at https://blog.tinned-software.net/rewrite-author-of-entire-git-repository/
 
-function git-replace-author() {
-
-if [[ $#  -ne 3 ]]; then
+usage () {
   local usage="Invalid input Please use the correct inputs
-  git-replace-author <old_email> <new_email> <new_name>"
 
-  echo -e "\\033[32;1m[INFO]\\033[0m ${usage}"
-fi
-if [[ $# -eq 3 ]]; then
-  git filter-branch --env-filter '
-  if [ "$GIT_COMMITTER_EMAIL" = "'$1'" ]; then
-    export GIT_COMMITTER_NAME='$2'
-    export GIT_COMMITTER_EMAIL='$3'
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "'$1'" ]; then
-  export GIT_AUTHOR_NAME='$2'
-  export GIT_AUTHOR_EMAIL='$3'
-    fi
-    ' --tag-name-filter cat -- --all
-  else
-    echo "usage: $0 old_email new_email new_name"
-    echo ""
-    echo "    OLD_EMAIL     The email address to be replaced in the commits"
-    echo "    NEW_EMAIL     The new author email address to be used in the commit matching OLD_EMAIL"
-    echo "    NEW_NAME      The name which should be used in the commit matching OLD_EMAIL"
-fi
+  git-replace-author <old_email> <new_email> <new_name>
+
+  OLD_EMAIL     The email address to be replaced in the commits
+  NEW_EMAIL     The new author email address to be used in the commit matching OLD_EMAIL
+  NEW_NAME      The name which should be used in the commit matching OLD_EMAIL"
+
+  echo  "\\033[32;1m[INFO]\\033[0m ${usage}"
 }
 
-git-replace-author $1 $2 $3
+if [[ $#  -ne 3 ]]; then
+  usage
+  exit 1
+fi
+git filter-branch --env-filter '
+if test "$GIT_AUTHOR_EMAIL" = "$1"
+then
+  GIT_AUTHOR_EMAIL="$2"
+  GIT_AUTHOR_NAME="$3"
+  fi
+  if test "$GIT_COMMITTER_EMAIL" = "$1"
+  then
+    GIT_COMMITTER_EMAIL="$2"
+    GIT_COMMITTER_NAME="$3"
+  fi
+  ' -- --all
