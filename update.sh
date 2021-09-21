9f6d416e93bf9bb8a03f29960646583a6cf331d7
Josh Beauregard <sanguis@users.noreply.github.com>
2021-09-21 10:13:16 -0400

    Adding gitconfig for all submodules

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 if [ -z $P ]; then
   P=$HOME/.sanguis_settings
@@ -9,3 +9,18 @@ function update_ss() {
   git pull
   git submodule update --init --recursive
 }
+function set_git() {
+ cd $P/$1
+ git config user.email "sanguis@users.noreply.github.com"
+ git config user.name "Josh Beauregard"
+ cd $P
+}
+
+function set_git_dirs() {
+local dirs=$(git config --file .gitmodules --get-regexp path | awk '{ print $2 }')
+for d in ${dirs}; do
+
+  set_git $d
+done
+}
+set_git_dirs
