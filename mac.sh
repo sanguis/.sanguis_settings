773faff10d640aa61805a460d59afba68481efaf
Josh Beauregard <sanguis@users.noreply.github.com>
2021-09-27 09:05:16 -0400

    Added checkov and pre-commit to  mac install
    - https://github.com/bridgecrewio/checkov
    - https://pre-commit.com/

--- mac.sh
+++ mac.sh
@@ -3,10 +3,11 @@
 
 if [ ! -x brew ]
 then
+  echo "Homebrew missing installing"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 brew install bash zsh tmux source-highlight
 xcode-select --install
 
-brew install tmux python3 macvim bash
+brew install tmux python3 macvim bash coreutils grep gnu-sed gun-indent checkov pre-commit
