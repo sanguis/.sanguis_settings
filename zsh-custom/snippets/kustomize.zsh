1aa82df28decda76c7e32a01b181e29aa9a6eea1
Josh Beauregard <sanguis@users.noreply.github.com>
2021-10-14 09:34:16 -0400

    Fixing base file function

--- zsh-custom/snippets/kustomize.zsh
+++ zsh-custom/snippets/kustomize.zsh
@@ -1,6 +1,10 @@
 #!/bin/zsh
 
-kust_add () {
+_create_kust() {
   [[ ! -f kustomize.yaml ]] && touch kustomize.yaml
-  kustomize edit add $1
+}
+kust_add_base () {
+[[ -z $1 ]] && echo -e "\033[31;1m[ERROR]\033[0m please list files you want to add to the kustomize base" && return 1
+  _create_kust
+  kustomize edit add base $@
 }
