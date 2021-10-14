e435fc6b1622e469a0cc6d1ffebd392836f0b6d0
Josh Beauregard <sanguis@users.noreply.github.com>
2021-10-14 09:46:06 -0400

    kust_add_base adds file to git

--- zsh-custom/snippets/kustomize.zsh
+++ zsh-custom/snippets/kustomize.zsh
@@ -7,4 +7,5 @@ kust_add_base () {
 [[ -z $1 ]] && echo -e "\033[31;1m[ERROR]\033[0m please list files you want to add to the kustomize base" && return 1
   _create_kust
   kustomize edit add base $@
+  git add kustomize.yaml
 }
