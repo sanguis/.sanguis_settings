b2e0ef947e093765a45e4d47d2724bd50ae79ba3
Josh Beauregard <sanguis@users.noreply.github.com>
2021-10-20 08:13:49 -0400

    A of random changes that need to be captured or lost

--- zsh-custom/snippets/kustomize.zsh
+++ zsh-custom/snippets/kustomize.zsh
@@ -10,34 +10,34 @@ kust_add_base () {
   git add kustomize.yaml
 }
 
-kust_add_patch_dir() {
-_USAGE="Usage : kust_add_patch_dir  [options] [--] {new dir} {path to base dir}
-Adds directory and kustomzation.yaml to that directory
-Options:
--h|help       Display this message
-"
-
-while getopts ":h" opt
-do
-  case $opt in
-
-    h|help     )  echo $_USAGE; return 0   ;;
-
-    * ) echo -e "\033[31;1m[ERROR]\033[0m Option does not exist : $OPTARG\n"
-      echo ${usage}; return 1   ;;
-
-    esac    # --- end of case ---
-  done
-  shift $(($OPTIND-1))
-  [[ -z $1 ]] && echo $_USAGE && return 1
-  [[ -z $2 ]] && echo $_USAGE && return 1
-  [[ -f $1/kustomzation.yaml ]] && echo -e "\033[31;1m[ERROR]\033[0m patch directory already exists." && return 1
-  base_path=realpath --relative-to=$1 $2
-  mkdir $1
-  echo "
-  ---
-  apiVersion: kustomize.config.k8s.io/v1beta1
-  kind: Kustomization
-  resources:
-  - $base_path" >> $1/kustomzation.yaml
-}
+#kust_add_patch_dir() {
+#_USAGE="Usage : kust_add_patch_dir  [options] [--] {new dir} {path to base dir}
+#Adds directory and kustomzation.yaml to that directory
+#Options:
+#-h|help       Display this message
+#"
+#
+#while getopts ":h" opt
+#do
+#  case $opt in
+#
+#    h|help     )  echo $_USAGE; return 0   ;;
+#
+#    * ) echo -e "\033[31;1m[ERROR]\033[0m Option does not exist : $OPTARG\n"
+#      echo ${usage}; return 1   ;;
+#
+#    esac    # --- end of case ---
+#  done
+#  shift $(($OPTIND-1))
+#  [[ -z $1 ]] && echo $_USAGE && return 1
+#  [[ -z $2 ]] && echo $_USAGE && return 1
+#  [[ -f $1/kustomzation.yaml ]] && echo -e "\033[31;1m[ERROR]\033[0m patch directory already exists." && return 1
+#  base_path=realpath --relative-to=$1 $2
+#  mkdir $1
+#  echo "
+#  ---
+#  apiVersion: kustomize.config.k8s.io/v1beta1
+#  kind: Kustomization
+#  resources:
+#  - $base_path" >> $1/kustomzation.yaml
+#}
