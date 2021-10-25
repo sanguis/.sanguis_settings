5fcc492b1288f25bee753dd53b807c1117b98291
Josh Beauregard <sanguis@users.noreply.github.com>
2021-10-25 12:16:49 -0400

    Reverted bad push

--- install.sh
+++ install.sh
@@ -18,10 +18,10 @@ pip3 install powerline-status
 
 if [ -f "$HOME/.zshrc" ]; then
   echo "backing up zshrc"
-  ZSHDATA=`cat ~/.zshrc`
-  mv $HOME/.zshrc $HOME/.zshrc_old
+  ZSHDATA=$(cat ~/.zshrc)
+  mv "$HOME/.zshrc" "$HOME/.zshrc_old"
   rm $HOME/.zshrc
-echo $ZSHDATA >> $P/zshrc
+  echo $ZSHDATA >> "$P/zshrc"
 fi
 
 # setup localfiles to be symbolic links and kept in local version control
@@ -41,20 +41,20 @@ if [[ ! -d $LOCAL_CONFIGS ]]; then
   cd "$LOCAL_CONFIGS" || exit 1
   echo "creating local con figs git repo and adding symbolic links"
   git init
-  links "${1[@]}"
+  #links "${1[@]}"
   git add config
   git commit config --message "Adding empty config files"
-  cd $CUR
+  cd "$CUR" || exit 1
 
 fi
 #function_body
 }
-local_configs "${local_config_files[@]}"
+#local_configs "${local_config_files[@]}"
 # create symlinks
 
-source $P/links.sh
+source "$P/links.sh"
 
-links() {
+function links() {
   for k in $(links[@]); do
     [[ -f $(links[${k}]) ]] && return
     [[ $DEBUG ]] && echo "ln -s $P/${k} ${links[${k}]}"
