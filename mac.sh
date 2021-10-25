c4c19be316592aef9a7a1bbaac385083eb1ed9f1
Josh Beauregard <sanguis@users.noreply.github.com>
2021-10-25 11:47:01 -0400

    fixed brew install

--- mac.sh
+++ mac.sh
@@ -1,7 +1,6 @@
-
 #!/usr/bin/env bash
 
-if [ ! -x brew ]
+if [[ ! -x /usr/local/bin/brew ]]
 then
   echo "Homebrew missing installing"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
