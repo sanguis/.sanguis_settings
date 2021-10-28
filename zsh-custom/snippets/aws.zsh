74dedd1e4488dc6c1f3700e43140ffa7a8153e6f
Josh Beauregard <sanguis@users.noreply.github.com>
2021-10-28 09:25:13 -0400

    Adding ecr_login command

---
+++ zsh-custom/snippets/aws.zsh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+
+ecr_login() {
+  _USAGE="Usage : ecr_login  [-hr:] [--] {2:inputs}
+      logs into aws ecr based on the account id and default region.
+      defaults to accunt number of current aws cli profile
+      Options:
+      -h|help       Display this message
+      -r|region     AWS region
+  "
+
+  while getopts ":hr:" opt
+  do
+    case $opt in
+
+    h|help     )  echo $_USAGE; return 0   ;;
+    r|region   ) _REGION="${OPTARG}"       ;;
+
+
+    * ) echo -e "\033[31;1m[ERROR]\033[0m Option does not exist : $OPTARG\n"
+        echo $_USAGE; return 1   ;;
+
+    esac    # --- end of case ---
+  done
+  shift $(($OPTIND-1))
+  if [[ -z $1]]; then
+    _ACCOUNT_NUMBER=$(aws sts get-caller-identity --output text --query 'Account')
+  else
+    _ACCOUNT_NUMBER=$1
+  fi
+  [[ -z $_REGION ]]
+
+  local COMMAND="aws ecr get-login-password --region $_REGION| docker login --username AWS --password-stdin $_ACCOUNT_NUMBER.dkr.ecr.$_REGION.amazonaws.com"
+  [[ $DEBUG ]] && [[ ${DEBUG} ]] && echo -e "\033[34;1m[DEBUG]\033[0m Running command:
+  ${lCOMMAND}"
+  eval ${COMMAND}
+}
