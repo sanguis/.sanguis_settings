c47ca3b521d61adf15f9ad512de94c1f18c20166
Josh Beauregard <sanguis@users.noreply.github.com>
2021-10-28 09:57:02 -0400

    Fixing ecr_login

--- zsh-custom/snippets/aws.zsh
+++ zsh-custom/snippets/aws.zsh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+alias aws_envs="env |grep AWS"
 
 ecr_login() {
   _USAGE="Usage : ecr_login  [-hr:] [--] {2:inputs}
@@ -9,7 +10,7 @@ ecr_login() {
       -r|region     AWS region
   "
 
-  while getopts ":hr:" opt
+  while getopts ":hr" opt
   do
     case $opt in
 
@@ -23,15 +24,21 @@ ecr_login() {
     esac    # --- end of case ---
   done
   shift $(($OPTIND-1))
+  # REGION setting
+  [[ -z $_REGION ]] && [[ -z $AWS_DEFAULT_REGION ]] && echo -e "\033[31;1m[ERROR]\033[0m No region or default region set" && return 1
+  [[ -z $_REGION ]] && _REGION=$AWS_DEFAULT_REGION
+  [[ $DEBUG ]] && echo -e "\033[34;1m[DEBUG]\033[0m Region: $_REGION"
+
+  # Account number
   if [[ -z $1 ]]; then
     _ACCOUNT_NUMBER=$(aws sts get-caller-identity --output text --query 'Account')
   else
     _ACCOUNT_NUMBER=$1
   fi
-  [[ -z $_REGION ]]
 
-  local COMMAND="aws ecr get-login-password --region $_REGION| docker login --username AWS --password-stdin $_ACCOUNT_NUMBER.dkr.ecr.$_REGION.amazonaws.com"
-  [[ $DEBUG ]] && [[ ${DEBUG} ]] && echo -e "\033[34;1m[DEBUG]\033[0m Running command:
-  ${lCOMMAND}"
-  eval ${COMMAND}
+  [[ $DEBUG ]]  && echo -e "\033[34;1m[DEBUG]\033[0m AWS Accoount #: $_ACCOUNT_NUMBER"
+  _COMMAND="aws ecr get-login-password --region $_REGION| docker login --username AWS --password-stdin $_ACCOUNT_NUMBER.dkr.ecr.$_REGION.amazonaws.com"
+  [[ $DEBUG ]] && echo -e "\033[34;1m[DEBUG]\033[0m Running command:
+  $_COMMAND"
+  eval $_COMMAND
 }
