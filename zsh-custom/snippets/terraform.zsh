3c76398e4fcdbe2312b0d64c605c47777a613691
Josh Beauregard <sanguis@users.noreply.github.com>
2021-11-24 11:35:59 -0500

    Added submodule creation function

--- zsh-custom/snippets/terraform.zsh
+++ zsh-custom/snippets/terraform.zsh
@@ -1,4 +1,9 @@
 #!/usr/bin/env zsh
 #@brief Terraform extension functions
 
-:
+# @description call the current state of https://github.com/Diehlabs/terraform-module-scaffolding and addit it as a squashed subtree
+tf_new_module() {
+ local usage="tf_new_module module_name"
+ [[ -z $1 ]] && echo -e "\033[31;1m[ERROR]\033[0m Needs a module name. \n $usage" $$ return 1
+ git subtree pull --prefix $1 https://github.com/Diehlabs/terraform-module-scaffolding.git main --squash
+}
