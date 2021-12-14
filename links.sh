0e60c8c60f0f1a1a52a67b78d76b0ad43e095c78
Josh Beauregard <sanguis@users.noreply.github.com>
2021-12-14 08:43:51 -0500

    I starting ctags config

--- links.sh
+++ links.sh
@@ -6,4 +6,5 @@ links["vim"]=$HOME/.vim
 links["zshrc"]=$HOME/.zshrc
 links["tmux.conf"]=$HOME/.tmux.conf
 links["gitconfig"]=$HOME/.gitconfig
+links[".ctags"]=$HOME/.ctags
 links["app_configs/kitty.conf"]=$HOME/.config/kitty/kitty.conf
