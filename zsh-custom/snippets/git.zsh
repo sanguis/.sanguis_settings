610e767b44e77aacb851d41b4c0588dce89be231
Josh Beauregard <sanguis@users.noreply.github.com>
2022-01-11 09:53:43 -0500

    Adding gpa function"

--- zsh-custom/snippets/git.zsh
+++ zsh-custom/snippets/git.zsh
@@ -1,5 +1,29 @@
 #!/bin/zsh
 
 fetch_and_checkout() {
-  git fetch --all && git checkout $1
+git fetch --all && git checkout $1
+}
+
+
+# @description Pulls all repos in targeted directory
+#
+#  @example
+#     gup src
+#
+#  @arg $1 Directory to scan.
+#  @arf $2 Depth (defaults to 3)
+#
+#  @exitcode 0 If successful.
+#  @exitcode 1 If an empty string passed.)
+gup () {
+i
+
+  [[ -z $1 ]] &&
+  for i in $(find ~/Desktop/Repos -name .git -type d -maxdepth 3 | cut -f1 -d.)
+  do cd $i
+    echo $i
+    git branch
+    gp
+  done
+
 }
