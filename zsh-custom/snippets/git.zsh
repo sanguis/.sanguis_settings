7c727f0f56990e53c3bf5d902d40a7a24d8d9a9a
Josh Beauregard <sanguis@users.noreply.github.com>
2022-07-08 08:13:16 -0400

    Adding git_reclone function
    so that I can quickly delete and re-clone a
    dirty repo

--- zsh-custom/snippets/git.zsh
+++ zsh-custom/snippets/git.zsh
@@ -1,23 +1,27 @@
 #!/bin/zsh
+# @file git_snippets
+# @brief custom settings and alias's for the git cli
+# @description
+
 
 fetch_and_checkout() {
 git fetch --all && git checkout $1
 }
 
 
-# @description Pulls all repos in targeted directory
+# @description Recursively Pulls all repos in targeted directory
 #
 #  @example
-#     gup src
+#     gup_recursive src
 #
 #  @arg $1 Directory to scan.
 #  @arf $2 Depth (defaults to 3)
 #
 #  @exitcode 0 If successful.
 #  @exitcode 1 If an empty string passed.)
-gup () {
-  local USAGE='USAGE:
-  gup DIR DEPTHj (Optional)'
+gup_recursive() {
+  local USAGE="USAGE: gup DIR DEPTH
+  DEPTH defaults too 3 is optional "
   [[ -z $1 ]] && echo -e "\033[31;1m[ERROR]\033[0m Directory input required" && echo $USAGE && return 1
   [[ -z $2 ]] && local DEPTH=3 || local DEPTH=$2
 
@@ -25,7 +29,20 @@ gup () {
   do cd $i
     echo $i
     git branch
-    gp
+    git pull
   done
+}
 
+git_reclone() {
+
+repo=$1
+base=$(pwd)
+cd $repo
+git remote show origin
+git remote get-url origin
+remote-repo=$(git remote get-url origin)
+remote_repo=$(git remote get-url origin)
+cd $base
+rm -rf $repo
+git clone --recursive $remote_repo $repo
 }
