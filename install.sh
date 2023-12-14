440008dcaab18081c3ad4ec91a23d56706cfd19e
Josh Beauregard <sanguis@users.noreply.github.com>
2023-12-14 10:00:58 -0500

    Moved all actions into functions
    Added debug funtionality
    IStarted better file ingest of links

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 __ScriptVersion="rolling"
 
 #===  FUNCTION  ================================================================
@@ -13,6 +13,9 @@ function usage ()
 	-h|help       Display this message
 	-v|version    Display script version
 	-d|dir        Sets install dir defaults to $HOME/.sanguis_settings
+	-b|backup     Backup directory for existing files
+	-D|debug      Set debugging output
+
 	"
 }    # ----------  end of function usage  ----------
 
@@ -20,13 +23,15 @@ function usage ()
 #  Handle command line arguments
 #-----------------------------------------------------------------------
 
-while getopts ":hvd:" opt
+while getopts ":hvd:b:D" opt
 do
 	case $opt in
 
 		h|help     )  usage; exit 0   ;;
 		v|version  )  echo "$0 -- Version $__ScriptVersion"; exit 0   ;;
 		d|dir 		) P=$OPTARG		;;
+		b|backup	) local BACKUP_DIR=$OPTARG ;;
+		D|debug		) DEBUG=true ;;
 
 		* )  echo -e "\n  Option does not exist : $OPTARG\n"
 			usage; exit 1   ;;
@@ -34,37 +39,41 @@ do
 		esac    # --- end of case ---
 	done
 	shift $(($OPTIND-1))
+	## set defaults
+
 	[[ -z $P ]] && P=$HOME/.sanguis_settings
+	[[ -z $BACKUP_DIR ]] && BACKUP_DIR=$HOME/.pre_sang_dotfiles
+
 
 	OS=$(uname)
 
+clone() {
 	if [[ ! -d $P ]]
 	then
 		git clone --recursive https://github.com/sanguis/.sanguis_settings.git "$P"
 		cd "$P" || exit 1
 	fi
-
+}
+mac() {
 	[[ $OS == "Darwin" ]] && source "$P/mac.sh"
-
+}
+update() {
 	source update.sh "$P"
+}
+
 	## install powerline fonts
-	bash ./fonts/install.sh
+packages() {
+  bash ./fonts/install.sh
 
 	pip3 install powerline-status
 
-	if [ -f "$HOME/.zshrc" ]; then
-		echo "backing up zshrc"
-		ZSHDATA=$(cat ~/.zshrc)
-		mv "$HOME/.zshrc" "$HOME/.zshrc_old"
-		rm $HOME/.zshrc
-		echo $ZSHDATA >> "$P/zshrc"
-	fi
+}
 
 # setup localfiles to be symbolic links and kept in local version control
 # ~/.zshrc_user
 # ~/.ssh/config
 # TODO ~/.gitconfig
-
+local_configs(){
 declare -A local_config_files
 local_config_files['ssh_config']=$HOME/.ssh/config
 local_config_files['zshrc_user']=$HOME/.zshrc_user
@@ -83,20 +92,66 @@ function local_configs() {
 		cd "$CUR" || exit 1
 
 	fi
-	#function_body
 }
 #local_configs "${local_config_files[@]}"
 # create symlinks
+}
 
-source "$P/links.sh"
+get_links() {
+    local csv_file=$1
+    local assoc_array_name=$2
+    local column1_name=$3
+    local column2_name=$4
+
+    [[ ! -f $csv_file ]] && echo "links file not found." && return
+
+    declare -A $assoc_array_name
+local header_line=1
+    while IFS=, read -r $column1_name $column2_name; do
+        if [[ "$header_line" == "1"  ]]; then
+            header_line=0
+            continue
+        fi
+        $assoc_array_name[$column1_name]=$column2_name
+    done < "$csv_file"
+
+  if [[ $DEBUG  ]]; then
+        echo "CSV file '$csv_file' has been converted to associative array '$assoc_array_name'"
+        for key in "${!assoc_array_name[@]}"; do
+            echo "Key: $key, Value: ${assoc_array_name[$key]}"
+        done
+  fi
+}
 
 function links() {
-	for k in $(links[@]); do
-		[[ -f $(links[${k}]) ]] && return
-		[[ $DEBUG ]] && echo "ln -s $P/${k} ${links[${k}]}"
-		ln -s $P/${k} $(links[${k}])
+	[[ -z $1 ]] && echo "symlinks is empty or missing cant contunue" && return
+	local symlinks=$1
+
+	for k in $(symlinks[@]); do
+		local target=$P/${k}
+		local link=$(symlinks[${k}])
+		#backup files
+		if [ -f $link ]; then
+			[ ! -d $BACKUP_DIR ] && mkdir $BACKUP_DIR
+local backup_loc=$BACKUP_DIR/$link
+			echo "$Link exists. moving existing file to $backup_loc"
+			mv $link $backup_loc
+		fi
+#create symlinks
+		[[ $DEBUG ]] && echo "ln -s ${target} ${link}"
+		ln -s ${target} ${link}
 	done
 }
-links
 
+# Run all this if not debugging
+if [[ ! $DEBUG ]]; then
+ #clone
+ #update
+ #packages
+ #mac
+ # local_configs
+  get_links "$P/links.csv" "symlinks" "target" "link"
+  links $symlinks
+	# source zshrc to start
 source "$HOME/.zshrc"
+fi
